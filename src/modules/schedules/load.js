369718907eafa87af05da5d9211189576903008a
Anaelica <anaanaelica@gmail.com>
2026-04-28 14:02:23 -0300

    renderizando os agendamentos da api

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,15 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 const selectedDate = document.getElementById("date")
 
-export function schedulesDay() {
+export async function schedulesDay() {
     //Obtem a data selecionada
     const date = selectedDate.value
+    
+    //buscar na api os agendamentos
+    const dailySchedules = await scheduleFetchByDay({date})
+    console.log (dailySchedules)
+
     hoursLoad({date})
 }
